daa36ad1dca40f754d254edd942eed52eb8bf743
Guy Eisenbach <guyeise5@gmail.com>
2023-04-01 17:07:40 +0300

    supporting limited services

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ const app = express()
 const port = Number(process.env.PORT) || 8080
 
 app.get("/.well-known/fastly/logging/challenge", (req, res) => {
-    res.status(200).send("*")
+    const services = process.env.FASTLY_HASHED_SERVICES;
+    if(services) {
+      res.status(200).end(services.split(",").join("\n"))
+    } else {
+      res.status(200).send("*");
+    }
 })
 app.use(express.text())
 app.post("*", (req, res) => {
