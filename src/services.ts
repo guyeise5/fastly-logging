060b3c08ae2e6dd39dea8ec41742c587502a7322
Guy Eisenbach <guyeise5@gmail.com>
2023-04-14 16:12:32 +0300

    remove logs

--- src/services.ts
+++ src/services.ts
@@ -6,6 +6,5 @@ export const services = (): string[] => {
   if(!data) {
      data = process.env.FASTLY_SERVICES?.split(",")?.map(s => CryptoJS.SHA256(s).toString(CryptoJS.enc.Hex)) || ["*"]
   } 
-  console.log(data)  
   return data;
 }
