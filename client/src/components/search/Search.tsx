f7d11326e155c9266ba258b0f69c3d9ab88cd20c
guyeisenbach <guy.eisenbach@humansecurity.com>
2024-03-31 20:16:29 +0300

    Saving the state as search params.

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -1,17 +1,27 @@
 import './Search.css'
 import {AiOutlineSearch} from 'react-icons/ai';
-import SearchType, {SearchTypeProps} from "../search-type/SearchType";
+import SearchType, {SearchOption, SearchTypeProps} from "../search-type/SearchType";
 
 type SearchProps = {
+    filter: string,
     updateFilter: (arg0: string) => void
     searchTypeProps: SearchTypeProps
+}
+
+export function toSearchOption(s: string | null): SearchOption {
+    switch (s) {
+        case "regexp":
+            return "regexp"
+        default:
+            return "words"
+    }
 }
 
 function Search(props: SearchProps) {
     return (
         <div className="search-bar">
             <SearchType option={props.searchTypeProps.option} setOption={props.searchTypeProps.setOption}/>
-            <input type="text" placeholder="Search" className="search-input"
+            <input type="text" placeholder="Search" className="search-input" value={props.filter}
                    onChange={(e) => props.updateFilter(e.target.value)}/>
             <AiOutlineSearch/>
         </div>
