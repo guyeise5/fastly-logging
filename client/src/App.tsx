37dba7e5c1809ed2fe3f107b08d0e856abad1763
guyeisenbach <guy.eisenbach@humansecurity.com>
2024-03-31 20:16:29 +0300

    Saving the state as search params.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,74 +1,95 @@
 import './App.css';
 import Table from './components/table/Table'
-import Search from './components/search/Search';
+import Search, {toSearchOption} from './components/search/Search';
 import {useEffect, useState} from "react";
 import {Message} from './types'
 import axios from 'axios';
 import Footer from "./components/footer/Footer";
 import TopButtons from "./components/top-buttons/TopButtons";
-import {SearchOption} from "./components/search-type/SearchType";
+import {useSearchParams} from "react-router-dom";
+
+const SEARCH_OPTION_QUERY_PARAM = "o";
+const FILTER_QUERY_PARAM = "s";
 
 function App() {
-  const [searchOption, setSearchOption] = useState<SearchOption>("words")
-  const [filter, setFilter] = useState<string>("");
-  const [messages, setMessages] = useState<Message[]>([]);
+    let [searchParams, setSearchParams] = useSearchParams();
+    const [searchOption, setSearchOption] = [
+        toSearchOption(searchParams.get(SEARCH_OPTION_QUERY_PARAM)),
+        (v: string) => {
+            setSearchParams(params => {
+                params.set(SEARCH_OPTION_QUERY_PARAM, v)
+                return params
+            })
+        }
+    ]
+    const [filter, setFilter] = [
+        searchParams.get(FILTER_QUERY_PARAM) || "",
+        (v: string) => {
+            setSearchParams(params => {
+                params.set(FILTER_QUERY_PARAM, v)
+                return params
+            })
+        }
+    ]
+    const [messages, setMessages] = useState<Message[]>([]);
 
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      try {
-        const messages = await fetchMessages()
-        setMessages(messages)
-      } catch(error) {
-        console.error("failed to fetch logs", error)
-      }
-    }, 1000)
-    return () => clearInterval(interval)
-  })
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            try {
+                const messages = await fetchMessages()
+                setMessages(messages)
+            } catch (error) {
+                console.error("failed to fetch logs", error)
+            }
+        }, 1000)
+        return () => clearInterval(interval)
+    })
 
-  const filteredMessages = filterMessages(messages, filter, searchOption);
+    const filteredMessages = filterMessages(messages, filter, searchOption);
     return (
-    <div>
-      <Search updateFilter={setFilter} searchTypeProps={{option: searchOption, setOption: setSearchOption}}/>
-        <TopButtons messages={filteredMessages}/>
-      <Table messages={filteredMessages} filter={{option: searchOption, text: filter}}/>
-        <Footer/>
-    </div>
-  );
+        <div>
+            <Search updateFilter={setFilter} filter={filter}
+                    searchTypeProps={{option: searchOption, setOption: setSearchOption}}/>
+            <TopButtons messages={filteredMessages}/>
+            <Table messages={filteredMessages} filter={{option: searchOption, text: filter}}/>
+            <Footer/>
+        </div>
+    );
 }
 
 async function fetchMessages(): Promise<Message[]> {
-  const url = (process.env.REACT_APP_SERVER_URL || "") + "/api/v1/log"
-  const response = await axios.get<Message[]>(url, { timeout: 1000 })
-  return response.data;
+    const url = (process.env.REACT_APP_SERVER_URL || "") + "/api/v1/log"
+    const response = await axios.get<Message[]>(url, {timeout: 1000})
+    return response.data;
 }
 
 function filterMessages(messages: Message[], filter: string, searchOption: string): Message[] {
-  switch (searchOption) {
-    case 'words':
-      return messages.filter(msg => filter?.split(' ')?.every(f => messageBodyAsString(msg).includes(f)))
-    case 'regexp':
-      return messages.filter(msg => toRegExp(filter).test(msg.message))
-    default:
-      throw new Error(`unsupported method ${searchOption}`)
-  }
+    switch (searchOption) {
+        case 'words':
+            return messages.filter(msg => filter?.split(' ')?.every(f => messageBodyAsString(msg).includes(f)))
+        case 'regexp':
+            return messages.filter(msg => toRegExp(filter).test(msg.message))
+        default:
+            throw new Error(`unsupported method ${searchOption}`)
+    }
 }
 
 export function toRegExp(s: string): RegExp {
-  try {
-    return new RegExp(s);
-  } catch (e) {
-    // A regexp that never matches
-    return /a^$/
-  }
+    try {
+        return new RegExp(s);
+    } catch (e) {
+        // A regexp that never matches
+        return /a^$/
+    }
 }
 
 
 export function messageBodyAsString(message: Message): string {
-  if(typeof message.message == "string") {
-      return message.message;
-  } else {
-      return JSON.stringify(message.message, undefined, 2)
-  }
+    if (typeof message.message == "string") {
+        return message.message;
+    } else {
+        return JSON.stringify(message.message, undefined, 2)
+    }
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path={"*"} element={<App/>}></Route>
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
